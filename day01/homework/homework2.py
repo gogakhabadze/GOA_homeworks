c58f907ec978885522441b6f84f7747cce73ef10
gogakhabadze <antpogyt2009@gmail.com>
2025-02-05 14:51:43 +0400

    second commit

---
+++ day01/homework/homework2.py
@@ -0,0 +1,34 @@
+name="goga"
+
+surname = "khabadze"
+
+# print(name)
+
+name = "goga" 
+age = 15
+height = 176
+
+knows_programming = True #true or false
+
+is_determined = True
+is_tired = False
+
+# print(name + " " + surname)
+# print(name + age)
+
+# print(type(age))
+# print(type(knows_programming))
+# print(type(name))
+# print(type(height))
+# print(type(is_determined))
+# print(type(is_tired))
+# print(type(surname))
+
+print(name + " " +surname + " is " + str(age) + " years old and is " + str(height) + " CM tall")
+# print(str(height) + " meters")
+
+
+
+
+
+
