463cb2188d6b72d6151c8c9c9aa42803a40d89ce
gogakhabadze <antpogyt2009@gmail.com>
2025-03-20 17:25:19 +0400

    commit

---
+++ day06/classwork/classwork.py
@@ -0,0 +1,10 @@
+#name = input (goga)
+#print (name)
+
+num1 = int (input ("Enter a number: "))
+num2 = int  (input ("Enter another number: "))
+
+print (num1 + num2)
+print (num1 - num2)
+print (num1 * num2)
+
