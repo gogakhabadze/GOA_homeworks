e2dee2186ccc3be9421be257b7135cfb2a5c601c
gogakhabadze <antpogyt2009@gmail.com>
2025-03-29 15:36:35 +0400

    commit

---
+++ day06/classwork/classwork1.py
@@ -0,0 +1,14 @@
+#რა არის Snake Case?
+#Snake case არის წერის სტილი, როდესაც სიტყვები იყოფა ქვედა ტირეს _ გამოყენებით და ყველა ასო არის პატარა:
+
+user_name = "John Doe"  # ეს არის snake_case
+
+#რა არის Case Sensitivity?
+#Case Sensitivity ნიშნავს იმას, რომ ასოების დიდსა და პატარა ვარიანტებს (uppercase/lowercase) განსხვავებული მნიშვნელობა აქვთ.
+
+name = "John"
+Name = "Doe"
+
+print(name)  # გამოიტანს "John"
+print(Name)  # გამოიტანს "Doe"
+
