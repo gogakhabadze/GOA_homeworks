5d521f725d20cefce9434b71a4cbc2ead2c6b57f
gogakhabadze <antpogyt2009@gmail.com>
2025-04-03 00:01:25 +0400

    commit

---
+++ day08/classwork/classwork1.py
@@ -0,0 +1,11 @@
+# name = str(input("თქვენი სახელი: "))
+# surname = str(input("თქვენი გვარი: "))
+
+# age = int(input("თქვენი ასაკი: "))
+
+
+# print(name + " " + surname + " " + str(age))
+
+
+message = "Debugging
+print(message)
