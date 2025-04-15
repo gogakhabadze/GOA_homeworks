3b48a658e240d24127d76b7b5c30523f41d66de2
gogakhabadze <antpogyt2009@gmail.com>
2025-04-15 13:35:46 +0400

    commit

---
+++ day10/classwork/classwork2.py
@@ -0,0 +1,11 @@
+# მომხმარებლისგან ორი რიცხვის შემოტანა
+num1 = float(input("შეიყვანეთ პირველი რიცხვი: "))
+num2 = float(input("შეიყვანეთ მეორე რიცხვი: "))
+
+# შედარება მკაცრი ოპერატორებით
+print(f"{num1} < {num2}: {num1 < num2}")
+print(f"{num1} > {num2}: {num1 > num2}")
+print(f"{num1} <= {num2}: {num1 <= num2}")
+print(f"{num1} >= {num2}: {num1 >= num2}")
+print(f"{num1} == {num2}: {num1 == num2}")
+print(f"{num1} != {num2}: {num1 != num2}")
